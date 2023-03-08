c416ca793d98614fd0800dec74aec693243dc637
Gulbius <mantas.gulbickas@gmail.com>
2023-03-08 18:42:27 +0200

    empty spaces

--- cw.php
+++ cw.php
@@ -28,6 +28,8 @@ function chromosomeCheck($str) {
       return "Congratulations! You're going to have a son.";
     }
   }
+
+  
 //////////////Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 function even_or_odd(int $n): string {
